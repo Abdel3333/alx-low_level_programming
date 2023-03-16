bd54565bc80dd25f83b3c8ea763f75a19eeed40f
Abdel3333 <sabirou94@gmail.com>
2023-03-16 12:44:33 -0700

    add aditionnal tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-	/**
-	 * main - this is the principal function
-	 * Return: 0
-	 */
-	int main(void)
-	{
-		puts("Programming is like building a multilingual puzzle");
-		return (0);
-	}
+
+/**
+ * main - this is the principal function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
