79527933cd9d4d1f15d9cd5309096ae89c93af29
Abdel3333 <sabirou94@gmail.com>
2023-03-22 15:48:18 -0700

    add aditionnal tasks

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print sum of multiple of 3 and 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 15 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
