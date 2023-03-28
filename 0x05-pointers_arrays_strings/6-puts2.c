82e9d96a5c2836097a729a6dc69732a31dea637f
Abdel3333 <sabirou94@gmail.com>
2023-03-28 10:18:26 -0700

    add aditionnal tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,19 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, strlen = 0;
 
+	while (*(str + strlen) != '\0')
+	{
+		strlen++;
+	}
 	while (*(str + i) != '\0')
 	{
 		_putchar(*(str + i));
 		i += 2;
 	}
+	if (strlen % 2 == 0)
+		_putchar('\0');
+	
 	_putchar('\n');
 }
