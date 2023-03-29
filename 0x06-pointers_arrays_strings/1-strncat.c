8035350a676be6f05b0dc66041bae94713a0a57b
Abdel3333 <sabirou94@gmail.com>
2023-03-29 10:02:20 -0700

    add aditionnal tasks

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+/**
+ * *_strncat - concatenate two strings
+ * @dest: first string
+ * @src: second string
+ * @n: numbers of bytes
+ * Return: A pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, k = 0;
+
+	for (i = 0; *(dest + i); i++)
+		;
+	for (j = i; k < n; j++)
+	{
+		*(dest + j) = *(src + k);
+		k++;
+	}
+	*(dest + (j + 1)) = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * *_strncpy - copy a string
+ * @dest: the pointer to dest
+ * @str: the pointer to the string
+ * @n: the number of bytes
+ * Return: the dest pointer
+ */
+char *_strncpy(char *dest, char *str, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *(str + i) != '\0'; i++)
+		*(dest + i) = *(str + i);
+	*(dest + i) = '\0';
+
+	return (dest);
+}
