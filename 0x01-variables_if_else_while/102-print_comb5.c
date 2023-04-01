c13527b7fa45c7f870f5148098df8c272f326d86
Abdel3333 <sabirou94@gmail.com>
2023-04-01 05:33:32 -0700

    add aditionnal tasks

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: an integer
+ */
+int main(void)
+{
+	int i = 0, j = 0, k = 0, l = 0, comb = 0;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			while (k < 10)
+			{
+				while (l < 10)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(32);
+					putchar(k + '0');
+					putchar(l + '0');
+					if (!(i == 9 && j == 9 && k == 9 && l == 9))
+					{
+						putchar(44);
+						putchar(32);
+					}
+					l++;
+				}
+				l = 0;
+				k++;
+			}
+			k = 0;
+			j++;
+		}
+		j = 0;
+		i++;
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
