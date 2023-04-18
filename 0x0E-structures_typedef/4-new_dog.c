297a06a6030b4bff0098b160cd1f34690639b776
abdel3 <sabirou94@gmail.com>
2023-04-18 18:25:08 +0100

    finish with the first mandatory tasks typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,12 +14,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(*d));
 	if (d == NULL)
 		return (NULL);
-	if (name != NULL && owner != NULL && age != NULL)
+	if (name != NULL && owner != NULL)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 		return (d);
 	}
+	free(d);
 	return (NULL);
 }
