7d6d6fa8386dc874fe3c9c2ed1cbc6da4283649b
abdel3 <sabirou94@gmail.com>
2023-04-18 04:05:18 +0100

    finish with the first mandatory tasks

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * *new_dog - creates a ne dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: a dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	d = malloc(sizeof(*d));
+	if(d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
