68a76e496b6a8edd9c61614703a1b55900eac43f
abdel3 <sabirou94@gmail.com>
2023-04-22 00:15:20 +0100

    finish with the first mandatory tasks typedef

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,8 +14,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 
 	va_start(args, n);
-	if (n == 0)
-		printf("");
 	while (i < (int) n)
 	{
 		s = va_arg(args, char*);
